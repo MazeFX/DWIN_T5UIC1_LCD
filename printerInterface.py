49be9cfc3d8eeefd51af26425367aa5b6795a02f
MazeFX <filoplast@gmail.com>
2024-06-03 12:26:14 +0200

    Changed user based file path for klippy socket

--- printerInterface.py
+++ printerInterface.py
@@ -253,7 +253,8 @@ class PrinterData:
 		self.op = MoonrakerSocket(URL, 80, API_Key)
 		self.status = None
 		print(self.op.base_address)
-		self.ks = KlippySocket('/home/pi/printer_data/comms/klippy.sock', callback=self.klippy_callback)
+		# TODO - make this user agnostic
+		self.ks = KlippySocket('/home/mazefx/printer_data/comms/klippy.sock', callback=self.klippy_callback)
 		subscribe = {
 			"id": 4001,
 			"method": "objects/subscribe",
